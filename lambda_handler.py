5ac916d7d94ff92437555f365ba0c910ed4d2640
rajsr1 <sristi.raj@novartis.com>
2022-07-14 22:26:05 +0530

    Added

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,5 +1,4 @@
 import json
-import requests
 from datetime import datetime
 from urllib.error import HTTPError
 import boto3
@@ -36,7 +35,8 @@ def lambda_handler(event, context):
     cluster_id=secret_json['dbClusterIdentifier']    
     headers = event.get("headers","unknown")
     query_string_params = event.get("queryStringParameters","unknown")
-    
+    print(headers)
+    print(query_string_params)
     bc_session = bc.get_session()
     
     session = boto3.Session(
@@ -48,18 +48,51 @@ def lambda_handler(event, context):
     client_redshift = session.client("redshift-data")
     print("Data API client successfully loaded")
     
-    query_str = "select  part_ssn, post_date, sum(employee) employee, sum(automatic) automatic, sum(matching) matching, sum(row_total) row_total from {} where PART_SSN='{}' and post_date between to_date('{}','YYYY-MM-DD') and to_date('{}','YYYY-MM-DD') group by part_ssn, post_date, activity, fund, ae, rv;".format(table_name, query_string_params["ssn"], query_string_params["start_date"], query_string_params["end_date"])
+    query_str = "select  part_ssn, sum(employee_contribution) employee, sum(agency_automatic) automatic, sum(matching_dollar_contribution) matching, sum(row_total) row_total from {} where PART_SSN='{}'  group by part_ssn".format(table_name, query_string_params["ssn"])
                       
     print(query_str)
     
     res = client_redshift.execute_statement(Database= db_name, SecretArn= secret_arn, Sql= query_str, ClusterIdentifier= cluster_id)
+    
     id=res["Id"]
     
+    query_status = client_redshift.describe_statement(Id=id)["Status"]
+
+    while query_status!='FINISHED' and query_status!='ABORTED' and query_status!='FAILED':
+
+        query_status = client_redshift.describe_statement(Id=id)["Status"]
+    
+    print(query_status)
+    if query_status == "FAILED":
+        print("Error Detail: "+client_redshift.describe_statement(Id=id)["Error"])
+    nextToken = query_string_params.get("nextToken",None)
+    response = None
+    
+    if nextToken is None and query_status=='FINISHED':
+        response = client_redshift.get_statement_result(
+            Id=id
+        )
+    elif query_status=='FINISHED':
+        response = client_redshift.get_statement_result(
+            Id=id,
+            NextToken=nextToken
+        )
+    
+    result = []
+    
+    for row in response["Records"]:
+        part_ssn = row[0][list(row[0].keys())[0]]
+        employee =   row[1][list(row[1].keys())[0]] if list(row[1].keys())[0]!="isNull" else "null"
+        automatic =  row[2][list(row[2].keys())[0]] if list(row[2].keys())[0]!="isNull" else "null"
+        matching =   row[3][list(row[3].keys())[0]] if list(row[3].keys())[0]!="isNull"  else "null"
+        row_tot =    row[4][list(row[4].keys())[0]] if list(row[4].keys())[0]!="isNull" else "null"
+        result.append({"ssn":part_ssn,"employee":employee,"automatic":automatic,"matching":matching,"row_total":row_tot})
+        
     response = {
         "isBase64Encoded":False,
         "statusCode": 200,
         "body":{
-            "row":id
+            "row": result
         }
     }
     
